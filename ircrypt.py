c022bfbf3d35ab810d7cb9b69179e2a95742b6ff
Sven Haardiek <Sven@Haardiek.de>
2014-03-10 17:46:43 +0100

    Started an IRCrypt xchat plugin.
    Should run:
    Symmetric encrytion and decrypion
    Save keys and load keys
    Set keys and remove keys
    
    But i think there are still a lot of bugs.
    
    Signed-off-by: Sven Haardiek <Sven@Haardiek.de>

---
+++ ircrypt.py
@@ -0,0 +1,298 @@
+__module_name__ = 'IRCrypt'
+__module_version__ = 'Snapshot'
+__module_description__ = 'IRCrypt-Lite: Encryption layer for IRC'
+
+ircrypt_help_text = '''
+Add, change or remove key for nick or channel.
+
+IRCrypt command options:
+
+list                                                 List set keys
+set-key         [-server <server>] <target> <key>    Set key for target
+remove-key      [-server <server>] <target>          Remove key for target
+
+Set the key for a channel:
+  /ircrypt set-key -server freenet #IRCrypt key
+Remove the key:
+  /ircrypt remove-key #IRCrypt
+Set the key for a user:
+  /ircrypt set-key nick key
+'''
+
+import string, os, subprocess, base64
+import time
+import xchat
+
+# Global buffers used to store message parts, pending requests, configuration
+# options, keys, etc.
+ircrypt_msg_buffer = {}
+ircrypt_keys = {}
+ircrypt_cipher = {}
+
+# Constants used throughout this script
+MAX_PART_LEN     = 300
+MSG_PART_TIMEOUT = 300 # 5min
+
+class MessageParts:
+	'''Class used for storing parts of messages which were split after
+	encryption due to their length.'''
+
+	modified = 0
+	last_id  = None
+	message  = ''
+
+	def update(self, id, msg):
+		'''This method updates an already existing message part by adding a new
+		part to the old ones and updating the identifier of the latest received
+		message part.
+		'''
+		# Check if id is correct. If not, throw away old parts:
+		if self.last_id and self.last_id != id+1:
+			self.message = ''
+		# Check if the are old message parts which belong due to their old age
+		# probably not to this message:
+		if time.time() - self.modified > MSG_PART_TIMEOUT:
+			self.message = ''
+		self.last_id = id
+		self.message = msg + self.message
+		self.modified = time.time()
+
+
+def ircrypt_decrypt_hook(word, word_eol, userdata):
+
+	global ircrypt_msg_buffer, ircrypt_keys
+
+	# There has to be a much nicer error treatment, but for now this works
+	if '>ACRY' or '>WCRY' in word_eol[0]:
+		return xchat.EAT_ALL
+
+	# get context
+	con = xchat.get_context()
+
+	# Get channel and server from context
+	channel = con.get_info('channel')
+	server = con.get_info('server')
+	nick = word[0]
+	target = '%s/%s' % (server, channel)
+
+	# Get key
+	key = ircrypt_keys.get('%s/%s' % (server, channel))
+	if key:
+		# if key exists and >CRY part of message start symmetric encryption
+		if '>CRY-' in word_eol[0]:
+
+			pre, message    = string.split(word_eol[0], '>CRY-', 1)
+			number, message = string.split(message, ' ', 1 )
+
+			# Get key for the message buffer
+			buf_key = '%s.%s.%s' % (server, channel, nick)
+
+			# Decrypt only if we got last part of the message
+			# otherwise put the message into a globa buffer and quit
+			if int(number) != 0:
+				if not buf_key in ircrypt_msg_buffer:
+					ircrypt_msg_buffer[buf_key] = MessageParts()
+				ircrypt_msg_buffer[buf_key].update(int(number), message)
+				return xchat.EAT_ALL
+
+			# Get whole message
+			try:
+				message = message + ircrypt_msg_buffer[buf_key].message
+			except KeyError:
+				pass
+
+			# Decrypt
+			p = subprocess.Popen(['gpg', '--batch',  '--no-tty', '--quiet',
+				'--passphrase-fd', '-', '-d'],
+				stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			p.stdin.write('%s\n' % key)
+			p.stdin.write(base64.b64decode(message))
+			p.stdin.close()
+			decrypted = p.stdout.read()
+			p.stdout.close()
+
+			# Get and print GPG errors/warnings
+			err = p.stderr.read()
+			p.stderr.close()
+			if err:
+				con.prnt('GPG reported error:\n%s' % err)
+
+			# Remove old messages from buffer
+			try:
+				del ircrypt_msg_buffer[buf_key]
+			except KeyError:
+				pass
+			con.prnt(userdata)
+			con.emit_print(userdata, nick, decrypted)
+
+			return xchat.EAT_ALL
+
+	# Not decrypted
+	return xchat.EAT_NONE
+
+def ircrypt_encrypt_hook(word, word_eol, userdata):
+
+	global ircrypt_keys, ircrypt_cipher
+
+	# Get context
+	con = xchat.get_context()
+
+	# Get channel and server from context
+	channel = con.get_info('channel')
+	server = con.get_info('server')
+	target = '%s/%s' % (server, channel)
+	if target in ircrypt_keys:
+
+		# encrypt message
+		p = subprocess.Popen(['gpg', '--batch',  '--no-tty', '--quiet',
+			'--symmetric', '--cipher-algo',
+			'TWOFISH',
+			'--passphrase-fd', '-'],
+			stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		p.stdin.write('%s\n' % ircrypt_keys[target])
+		p.stdin.write(word_eol[0])
+		p.stdin.close()
+		encrypted = base64.b64encode(p.stdout.read())
+		p.stdout.close()
+
+		# Get and print GPG errors/warnings
+		err = p.stderr.read()
+		p.stderr.close()
+		if err:
+			con.prnt('GPG reported error:\n%s' % err)
+
+		xchat.emit_print('Your Message', xchat.get_info('nick'), word_eol[0])
+
+		# If too long for one message, split and send
+		if len(encrypted) > MAX_PART_LEN:
+			xchat.command('PRIVMSG %s :>CRY-1 %s' % (channel, encrypted[MAX_PART_LEN:]))
+
+		# Send (rest)
+		xchat.command('PRIVMSG %s :>CRY-0 %s' % (channel, encrypted))
+
+		return xchat.EAT_ALL
+
+def ircrypt_notice_hook(word, word_eol, userdata):
+
+	# There has to be a much nicer error treatment, but for now this works
+	if '>ACRY' or '>WCRY' in word_eol[0]:
+		return xchat.EAT_ALL
+
+	return xchat.EAT_NONE
+
+
+def ircrypt_command_hook(word, word_eol, userdata):
+
+	global ircrypt_keys, ircrypt_cipher
+
+	# Get context
+	con = xchat.get_context()
+
+	# list
+	if len(word) == 1 or word[1] == 'list':
+	# Print keys and special cipher in current buffer
+		if ircrypt_keys:
+			con.prnt('\nKeys:')
+			for servchan,key in ircrypt_keys.iteritems():
+				con.prnt('%s : %s' % (servchan, key))
+
+		if ircrypt_cipher:
+			con.prnt('\nSpecial Cipher:')
+			for servchan,spcip in ircrypt_cipher.iteritems():
+				con.prnt('%s : %s' % (servchan, spcip))
+		return xchat.EAT_ALL
+
+	if not word[1] in ['list', 'buffer', 'set-key', 'remove-key',
+			'set-cipher', 'remove-cipher']:
+		con.prnt('Unknown command. Try  /help ircrypt')
+		return xchat.EAT_ALL
+
+	# Check if a server was set
+	if (len(word) > 3 and word[2] == '-server'):
+		server_name = word[3]
+		del word[3]
+		del word[2]
+	else:
+		# Try to determine the server automatically
+		server = con.get_info('server')
+
+	# All remaining commands need a server name
+	if not server:
+		# if no server was set print message in ircrypt buffer and throw error
+		con.prnt('Unknown Server. Please use -server to specify server')
+		return xchat.EAT_ALL
+
+	param = 'Not enough parameter. Try /help ircrypt'
+
+	# For the remaining commands we need at least one additional argument
+	if len(word) < 3:
+		con.prnt(param)
+		return xchat.EAT_ALL
+
+	target = '%s/%s' % (server, word[2])
+
+	# Set keys
+	if word[1] == 'set-key':
+		if len(word) < 4:
+			con.prnt(param)
+			return xchat.EAT_ALL
+		ircrypt_keys[target] = ' '.join(word[3:])
+		# Print status message to current buffer
+		con.prnt('Set key for %s' % target)
+		return xchat.EAT_ALL
+
+	# Remove keys
+	if word[1] == 'remove-key':
+		if len(word) < 3 :
+			con.prnt(param)
+			return xchat.EAT_ALL
+		# Check if key is set and print error in current buffer otherwise
+		if target not in ircrypt_keys:
+			con.prnt('No existing key for %s.' % target)
+			return xchat.EAT_ALL
+		# Delete key and print status message in current buffer
+		del ircrypt_keys[target]
+		con.prnt('Removed key for %s' % target)
+		return xchat.EAT_ALL
+
+	# Set special cipher for channel
+	if word[1] == 'set-cipher':
+		return xchat.EAT_ALL
+
+	# Remove secial cipher for channel
+	if word[1] == 'remove-cipher':
+		return xchat.EAT_ALL
+
+	# Error if command was unknown
+	return xchat.EAT_NONE
+
+
+def ircrypt_init():
+
+	global ircrypt_keys
+
+	# Open config file
+	f = open('%s/ircrypt.conf' % xchat.get_info('xchatdirfs'), 'r')
+	for line in f:
+		if line[0:4] == 'key:':
+			(prefix, target, key) = line.split(':',2)
+			ircrypt_keys[target] = key[0:-1]
+
+	xchat.prnt('IRCrypt re(loaded)')
+	return xchat.EAT_ALL
+
+def ircrypt_unload(userdata):
+	global ircrypt_keys
+
+	# Open config file
+	f = open('%s/ircrypt.conf' % xchat.get_info('xchatdirfs'), 'w')
+	for target in ircrypt_keys:
+		f.write('key:%s:%s\n' % (target, ircrypt_keys[target]))
+	return xchat.EAT_ALL
+
+ircrypt_init()
+xchat.hook_command('ircrypt', ircrypt_command_hook, help=ircrypt_help_text)
+xchat.hook_command('', ircrypt_encrypt_hook)
+xchat.hook_print('Channel Message', ircrypt_decrypt_hook, 'Channel Message')
+xchat.hook_print('Notice', ircrypt_notice_hook)
+xchat.hook_unload(ircrypt_unload)
